380a74c349b1be3dda740c3e4b4af6a45f8ef368
Rollulus <roelboel@xs4all.nl>
2016-05-13 13:56:07 +0200

    Move examples to own directory

--- examples/demo.lua
+++ examples/demo.lua
@@ -1,8 +1,8 @@
-function process(undesired)
+return pb.mapValues(function(undesired)
 	return {
 		valid = not undesired.notValid,
 		name = undesired.person.name:lower(),
 		fingers = undesired.fingers_lh + undesired.fingers_rh
 	}
-end
+end)
 
