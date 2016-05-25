6fa7c0e760a9f909f976066ad13ea8fe77e3cb53
Rollulus <roelboel@xs4all.nl>
2016-05-25 10:00:45 +0200

    Fix demo example

--- examples/demo/example.sh
+++ examples/demo/example.sh
@@ -1,1 +1,1 @@
-java -jar ../../target/kafka-streams-plumber-0.1-jar-with-dependencies.jar -i example-in -o example-out -p example.properties -l example.lua -t example.test.lua -d avro -s avro=example.desired.avsc
+../../plumber.sh -i example-in -o example-out -p example.properties -l example.lua -t example.test.lua -d avro -s avro=example.desired.avsc
